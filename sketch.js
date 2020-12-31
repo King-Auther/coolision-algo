e90283552e7b9c03a09b33e2461247257f22d56d
King-Auther <76525401+King-Auther@users.noreply.github.com>
2020-12-31 15:58:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var fixed_rect,moving_rect;
+function setup() {
+  createCanvas(800,400);
+  moving_rect=createSprite(400, 200, 80, 30);
+  moving_rect.shapeColor="red";
+
+  fixed_rect=createSprite(200, 200, 50, 50);
+  fixed_rect.shapeColor="red";
+}
+
+function draw() {
+  background(255,255,255);
+  moving_rect.x=mouseX; 
+  moving_rect.y=mouseY;
+  
+  if(moving_rect.x-fixed_rect.x<=
+    fixed_rect.width/2+moving_rect.width/2 &&
+    fixed_rect.x-moving_rect.x<=
+    fixed_rect.width/2+moving_rect.width/2&&
+    moving_rect.y-fixed_rect.y<=
+    fixed_rect.height/2+moving_rect.height/2&&
+    fixed_rect.y-moving_rect.y<=
+    fixed_rect.height/2+moving_rect.height/2){
+      moving_rect.shapeColor="green";
+      fixed_rect.shapeColor="green";
+    }
+    else{
+      moving_rect.shapeColor="red";
+      fixed_rect.shapeColor="red";
+    }
+  drawSprites();
+}
